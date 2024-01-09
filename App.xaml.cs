5a3fd4378a92df13cf12ef66f533d86d823b7617
Daniel Hughes <daniel.hughes@taitradio.com>
2024-01-09 16:18:23 +1300

    Added reproduction code

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EmptyCollectionCpuIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EmptyCollectionCpuIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
